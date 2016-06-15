c2dfea56e74036b821092d366ee4dabe8cc1155a
Fabio Marozzi <fmarozz@gmail.com>
2016-06-15 14:28:10 +0200

    Included Jira

---
+++ WebCons.Data/Data/Jira.cs
@@ -0,0 +1,16 @@
+﻿namespace WebCons.Data
+{
+    public class Jira : AnagraficaBase
+    {
+        public string TipoSegnalazione { get; set; }
+        public string Codice { get; set; }
+        public string Segnalante { get; set; }
+        public string Priorità { get; set; }
+        public string Stato { get; set; }
+        //public DateTime Creato { get; set; }
+        public string Assegnatario { get; set; }
+        public string Risoluzione { get; set; }
+        public string Aggiornato { get; set; }
+
+    }
+}
